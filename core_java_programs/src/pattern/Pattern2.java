af693aedee622599e09d12371d6c0d3b069d371f
Shrinidhi <Shrinidhi@Smtc-R-0208>
2023-03-30 11:46:30 +0530

    Second pattern program in core java programs repo

---
+++ core_java_programs/src/pattern/Pattern2.java
@@ -0,0 +1,21 @@
+package pattern;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		int num=1;
+		for (int i = 0; i <4 ; i++) {
+			int num1=num;
+			for (int j = 0; j < 4; j++) {
+				System.out.print(num1+" ");
+				
+				
+			}
+			System.out.println();
+			num++;
+			
+		}
+
+	}
+
+}
